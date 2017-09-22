b103b24bcb5abf82249cbef7cd496b4344d59ecf
Ke Ma <kema2012@gmail.com>
2017-09-22 14:26:53 +0100

    add client code

---
+++ js/support.js
@@ -0,0 +1,29 @@
+/*
+Abstract:
+A helper function that requests an Apple Pay merchant session using a promise.
+*/
+
+function getApplePaySession(url) {
+    return new Promise(function (resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', '/getApplePaySession');
+      xhr.onload = function () {
+        if (this.status >= 200 && this.status < 300) {
+          resolve(JSON.parse(xhr.response));
+        } else {
+          reject({
+            status: this.status,
+            statusText: xhr.statusText
+          });
+        }
+      };
+      xhr.onerror = function () {
+        reject({
+          status: this.status,
+          statusText: xhr.statusText
+        });
+      };
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.send(JSON.stringify({url: url}));
+    });
+  }
