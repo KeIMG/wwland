da0a9d6db6a223ead451e98a9fa874add08a6ae6
Ke Ma <kema2012@gmail.com>
2017-09-26 14:08:42 +0100

    add js log

--- js/support.js
+++ js/support.js
@@ -6,7 +6,8 @@ A helper function that requests an Apple Pay merchant session using a promise.
 function getApplePaySession(url) {
     return new Promise(function (resolve, reject) {
       var xhr = new XMLHttpRequest();
-      xhr.open('GET', 'https://wwland.azurewebsites.net/applepay_includes/apple_pay_conf.php?u='.url);
+      console.log(url);
+      xhr.open('GET', 'https://wwland.azurewebsites.net/applepay_includes/apple_pay_conf.php?u=' + url);
       xhr.onload = function () {
         if (this.status >= 200 && this.status < 300) {
           resolve(JSON.parse(xhr.response));
