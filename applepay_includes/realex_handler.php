2cbd5c7e7736b3ff2b6cc98fa9f22dcc7287a3b3
Ke Ma <kema2012@gmail.com>
2017-10-10 16:15:27 +0100

    add shared secret

--- applepay_includes/realex_handler.php
+++ applepay_includes/realex_handler.php
@@ -10,31 +10,32 @@ use com\realexpayments\remote\sdk\domain\payment\PaymentRequest;
 use com\realexpayments\remote\sdk\domain\payment\PaymentResponse;
 use com\realexpayments\remote\sdk\domain\payment\PaymentType;
 use com\realexpayments\remote\sdk\RealexClient;
+use com\realexpayments\remote\sdk\http\HttpConfiguration;
 
 $token = $_POST['paymentToken'];
 
 $applePayRequest = (new PaymentRequest())
         ->addType(PaymentType::AUTH_MOBILE)
         ->addMerchantId(PRODUCTION_MERCHANTIDENTIFIER)
-        ->addAccount("internet")
+        ->addAccount("remote")
         ->addMobile("apple-pay")
         ->addToken($token)
         ->addAutoSettle((new AutoSettle())->addFlag(AutoSettleFlag::TRUE));
 
 $httpConfiguration = new HttpConfiguration();
 $httpConfiguration->setEndpoint("https://api.sandbox.realexpayments.com/epage-remote.cgi");
-$client = new RealexClient("Shared Secret", $httpConfiguration);
-$result = "";
+$client = new RealexClient("secret", $httpConfiguration);
+$result = '{"RealexHandlerError":"Unknown Error"}';
 try {
       $paymentResponse = $client->send($applePayRequest);
       $resultCode = $paymentResponse->getResult();
       $result = $paymentResponse->getMessage();
 }
 catch (RealexServerException $e) {
-      $result = $e->getMessage();
+    $result = '{"RealexServerException":"' . $e->getMessage() . '"}';
 }
 catch (RealexException $e) {
-      $result = $e->getMessage();
+      $result = '{"RealexException":"' . $e->getMessage() . '"}';
 }
 echo $result;
 
